5c1c8c0d258c7e3bbd0a664ae3dbd19e7e24e34f
SniperXu <398622064@qq.com>
2024-07-18 00:14:02 +0800

    :sparkles: feat:add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button, Result } from "antd";
+
+export default function NotFound() {
+    return (
+        <div className="flex-center">
+            <Result
+                status="404"
+                title="404"
+                subTitle="抱歉，您访问的页面不存在。"
+                extra={
+                    <Button type="primary" href="/">
+                        返回首页
+                    </Button>
+                }
+            />
+        </div>
+    );
+}
