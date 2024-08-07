0740ff2fb05610123adc2b79cc7e13a99b2e6c48
SniperXu <398622064@qq.com>
2024-08-08 00:24:02 +0800

    :computer: wip:update some change

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -6,3 +6,10 @@ export function GET_BANNER_ALL<T = unknown>() {
         headers: {}
     });
 }
+
+// 获取全部全局配置项
+export function GET_GLOBAL_ALL<T = unknown>() {
+    return httpRequest<T>("/settings/global/all", "get", {
+        headers: {}
+    });
+}
