cf0564c9f8224ce7ba47cd872ba687783f0c66c3
SniperXu <398622064@qq.com>
2024-08-08 22:17:37 +0800

    :computer: wip:update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,29 @@
 import type { Metadata } from "next";
+import { Card, Divider, Space, Tag } from "antd";
+import { GET_GLOBAL_ALL } from "@/api/settings";
+import { SettingsGlobalProps } from "#/settings";
+import dayjs from "dayjs";
 
 export const metadata: Metadata = {
     title: "关于我们",
     description: "一个Next.js快速启动模板"
 };
 
-const About: React.FC = () => {
+const About: React.FC = async () => {
+    // 获取全局配置项
+    const settingsRes = await GET_GLOBAL_ALL<SettingsGlobalProps[]>();
+    const settings = settingsRes.data.data ?? [];
+    const settingAbout = settings.find((item) => item.key === "about")?.value ?? "";
+
     return (
         <div className="page-container py">
-            <h1>关于我们</h1>
-            <p>这是一个关于我们页面</p>
+            <Card>
+                <div>
+                    <h1 className="text-24px font-bold">关于我们</h1>
+                    <Divider />
+                </div>
+                <div className="mt richText-container" dangerouslySetInnerHTML={{ __html: settingAbout }}></div>
+            </Card>
         </div>
     );
 };
